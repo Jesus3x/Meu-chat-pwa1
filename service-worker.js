5dfeadfce7917a2298ea994b74d7cd416a038edc
Jesus3x <lpbs10x@gmail.com>
2025-06-18 17:38:43 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,25 +1,23 @@
-const CACHE_NAME = 'chat-cache-v1';
-const urlsToCache = [
+const CACHE_NAME = 'chat-pwa-v1';
+const FILES_TO_CACHE = [
   './',
   './index.html',
   './manifest.json',
-  './icons/icon-192x192.png',
-  './icons/icon-512x512.png',
-  // Adicione aqui outros arquivos que desejar manter em cache
+  './icon-192x192.png',
+  './icon-512x512.png'
 ];
 
+// Instala o Service Worker
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
   );
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
-  );
-});
-
+// Ativa o Service Worker
 self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(keyList =>
@@ -31,5 +29,15 @@ self.addEventListener('activate', event => {
         })
       )
     )
+  );
+  self.clients.claim();
+});
+
+// Busca recursos no cache ou na rede
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
